0539cfb57d1fe33fafe2efda94c220f13876a91d
Fernando Medina <fmedina771@gmail.com>
2018-01-21 14:50:12 -0500

    passing methods and two way binding

--- src/App.js
+++ src/App.js
@@ -11,28 +11,47 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
       // console.log("was clicked");
       //DONT DO THIS TO SWTICH STATE: this.state.persons[0].name = "Vanessa Batista";
       this.setState({persons: [
-        {name: "Vanessa Batista", age: 24},
+        {name: newName, age: 24},
         {name: "Fernando Medina", age: 25},
         {name: "Kelvin Batista", age: 21}
       ]
-    })
-  }
+    });
+  };
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+      {name: 'Monkey', age:12},
+      {name: event.target.value, age:13},
+      {name: 'Dog', age:10}
+      ]
+
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1> Hi, This is my React app </h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age ={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age= {this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age= {this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Ana Medina!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age ={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age= {this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Sasha')}
+          changed={this.nameChangedHandler}>My Hobbies: Coding</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age= {this.state.persons[2].age}/>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
